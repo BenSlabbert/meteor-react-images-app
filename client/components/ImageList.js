a444b468ea7ec8356fe4746fa643cb91f3aae733
Ben Slabbert <ben@hex.co.za>
2017-12-18 10:21:54 +0200

    adding dummy images

--- client/components/ImageList.js
+++ client/components/ImageList.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ImageDetail from './ImageDetail';
 
+const IMAGES = [
+    { title: 'pen', link: 'https://dummyimage.com/600x400' },
+    { title: 'guitar', link: 'https://dummyimage.com/600x400' },
+    { title: 'hat', link: 'https://dummyimage.com/600x400' }
+];
+
 const ImageList = () => {
     return (
         <ul>
