ad4704ea18b1b9f0f24164ec6daf4b739313e7e4
Ben Slabbert <ben@hex.co.za>
2017-12-18 10:18:07 +0200

    adding image detail cleaning up main

---
+++ client/components/ImageDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImageDetail = () => {
+    return (
+        <div>
+            Image detail
+        </div>
+    );
+};
+
+export default ImageDetail;
--- client/main.js
+++ client/main.js
@@ -6,11 +6,8 @@ import ImageList from './components/ImageList';
 const App = () => {
     return (
         <div>
-            React component
-            <div>
-                <h1>List of images</h1>
-                <ImageList/>
-            </div>
+            <h1>List of images</h1>
+            <ImageList/>
         </div>
     )
 };
