ecdf70a8947e164ff3bc26c842cdc3f7e72ca98a
Ben Slabbert <ben@hex.co.za>
2017-12-18 10:44:52 +0200

    adding bootstrap and ome styles

--- client/components/ImageList.js
+++ client/components/ImageList.js
@@ -12,7 +12,7 @@ const ImageList = () => {
     const RenderedImages = IMAGES.map( image => <ImageDetail key={image.title} image={image}/> );
 
     return (
-        <ul>
+        <ul className="media-list list-group">
             {RenderedImages}
         </ul>
     );
