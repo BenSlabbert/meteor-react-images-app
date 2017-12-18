de161e655db9ad228d23d76a8bceb5deb01d5ae0
Ben Slabbert <ben@hex.co.za>
2017-12-18 10:14:08 +0200

    adding a list component

--- client/main.js
+++ client/main.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ImageList from './components/ImageList';
+
 const App = () => {
     return (
         <div>
             React component
+            <div>
+                <h1>List of images</h1>
+                <ImageList/>
+            </div>
         </div>
     )
 };
